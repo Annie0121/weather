6dcb9e154f2ac149281c272e41a5875998267553
54Laichu <machi123j@gmail.com>
2024-07-16 00:42:22 +0800

    feat: city names and towns name

--- controllers/cityname_controller.py
+++ controllers/cityname_controller.py
@@ -1,12 +1,23 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from models.CityName import CityName
 
 router = APIRouter()
 
-@router.get("/cityname")
+@router.get("/city_names")
 async def index():
   try:
-    cityname = ["臺北市", "新北市", "桃園市"]
-    return {"data": cityname}
+    cityname = CityName.get_city_names()
+    return JSONResponse(status_code=200, content={"data": cityname})
+  except Exception as e:
+    return JSONResponse(status_code=500, content={"message": str(e)})
+
+@router.get("/{city_name}")
+async def show(city_name: str):
+  try:
+    towns = CityName.get_towns(city_name)
+    if len(towns) == 0:
+      return JSONResponse(status_code=404, content={"message": f"找不到 {city_name} 的鄉鎮資料"})
+    return JSONResponse(status_code=200, content={"data": towns})
   except Exception as e:
     return JSONResponse(status_code=500, content={"message": str(e)})
--- models/CityName.py
+++ models/CityName.py
@@ -1,2 +1,20 @@
+import json
+import os
+
+current_dir = os.path.dirname(__file__)
+city_towns_file = os.path.join(current_dir, '..', 'docs', 'city_towns.json')
+with open(city_towns_file, 'r', encoding='utf-8') as f:
+  city_towns = json.load(f)
+
 class CityName:
-  pass
+
+  @staticmethod
+  def get_city_names():
+    return list(city_towns.keys())
+
+  @staticmethod
+  def get_towns(city_name):
+    try:
+      return city_towns[city_name]
+    except KeyError:
+      return []
