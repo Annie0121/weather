88493b0e860f7c552b2710a82ee4c55fb66ebdac
vance <vance@vances-MacBook-Air.local>
2024-07-17 04:09:00 +0800

    update controller params

--- controllers/weekly_weather_controller.py
+++ controllers/weekly_weather_controller.py
@@ -4,10 +4,14 @@ import requests
 import logging
 from urllib.parse import quote
 from models.CityName import CityName
+import os
+from dotenv import load_dotenv
 
 router = APIRouter()
 
-CWB_API_KEY = "CWA-32805D52-091B-4553-93D4-F60AD3936AC8" #測試用 前端可先測試後再PR刪除即可
+load_dotenv()
+CWB_API_KEY = os.getenv('CWB_API_KEY')
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -47,24 +51,30 @@ async def get_weather(city_name: str, town_name: str):
         return JSONResponse(status_code=404, content={"message": f"找不到 {city_name} 的天氣API endpoint"})
 
     endpoint = city_to_api_endpoint[city_name]
-    url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/{endpoint}?Authorization={CWB_API_KEY}&elementName=T&locationName={quote(town_name)}"
+    params = {
+        "Authorization": CWB_API_KEY,
+        "elementName": "MaxAT,UVI,PoP12h,RH",
+        "locationName": town_name
+    }
+    
+    url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/{endpoint}"
 
     headers = {
         "Accept": "application/json",
     }
 
-    logger.info(f"請求 {city_name} 的天氣數據")
-    logger.info(f"URL: {url}")
+    # logger.info(f"請求 {city_name} 的天氣數據")
+    # logger.info(f"URL: {url}")
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
         data = response.json()
-        logger.info(f"收到 {city_name} 的回應")
-        logger.debug(f"回應數據: {data}")
+        # logger.info(f"收到 {city_name} 的回應")
+        # logger.debug(f"回應數據: {data}")
 
     except requests.exceptions.RequestException as e:
-        logger.error(f"獲取天氣數據時出錯: {e}")
+        # logger.error(f"獲取天氣數據時出錯: {e}")
         return JSONResponse(status_code=500, content={"message": str(e)})
 
     try:
@@ -79,20 +89,23 @@ async def get_weather(city_name: str, town_name: str):
             weather_data = []
 
             for element in town_data["weatherElement"]:
-                if element["elementName"] == "T":
+                if element["elementName"] in ["MaxAT", "UVI", "PoP12h", "RH"]:
                     for time_entry in element["time"]:
                         forecast = {
                             "startTime": time_entry["startTime"],
                             "endTime": time_entry["endTime"],
-                            "temperature": time_entry["elementValue"][0]["value"]
+                            "elementName": element["elementName"],
+                            "description": element["description"],
+                            "value": time_entry["elementValue"][0]["value"],
+                            "measures": time_entry["elementValue"][0]["measures"]
                         }
                         weather_data.append(forecast)
           
-            logger.info(f"回應處理成功，返回數據: {town_name}")
+            # logger.info(f"回應處理成功，返回數據: {town_name}")
             return {"city": city_name,"town": town_name, "weather": weather_data}
         else:
-            logger.error(f"資料結構錯誤")
+            # logger.error(f"資料結構錯誤")
             return JSONResponse(status_code=500, content={"message": "資料錯誤"})
     except Exception as e:
-        logger.error(f"處理天氣數據時出錯: {e}")
+        # logger.error(f"處理天氣數據時出錯: {e}")
         return JSONResponse(status_code=500, content={"message": str(e)})
